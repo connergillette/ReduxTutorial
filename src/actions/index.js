c9d4ed7de56787fa070e11807f392ca8acb03528
Conner Gillette <cg1100@uw.edu>
2018-01-07 16:49:26 -0800

    Add changes up to Lecture 47

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-    console.log('A book has been selected: ', book.title);
+    // selectBook is an ActionCreator, needs to return action
+    // Action must have 'type' property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
 }
