e7e389a8f911f2d5a4c4e25610e771b49ff61dc5
Conner Gillette <cg1100@uw.edu>
2018-01-10 11:38:50 -0800

    Add changes up to Lecture 51

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 1021},
+        {title: 'The Dark Tower', pages: 201},
+        {title: 'Eloquent Ruby', pages: 11}
     ]
 }
